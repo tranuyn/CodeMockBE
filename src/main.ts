390951682caf7a7a6048b09a4babf69df1c6287c
Hồ Kim Thiên Nga <127382679+cabungbu@users.noreply.github.com>
2025-04-23 22:11:15 +0700

    CORS

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true, // Bắt buộc nếu bạn dùng cookie hoặc JWT token qua header
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 8081);
 }
 bootstrap();
